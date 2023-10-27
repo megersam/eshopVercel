12a4b01975ed7f7cd9e37737ad03b701770c2c16
megersam <megersabmuleta01@gmail.com>
2023-10-27 22:10:50 +0300

    url changes

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['http://localhost:3000',],
+  origin: ['https://eshop-ten-rho.vercel.app',],
   credentials: true
 }));
 
